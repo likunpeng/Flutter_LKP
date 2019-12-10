d367a2c0fa58ee1d4ffb4a123fae075728dd37a5
likunpeng <360315247@qq.com>
2019-12-10 22:57:08 +0800

    test material app

--- studyflutter/lib/main.dart
+++ studyflutter/lib/main.dart
@@ -1,22 +1,42 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    title: 'Flutter Tutorial',
-    home: MyApp(),
-  ));
+//  runApp(MaterialApp(
+//    title: 'Flutter Tutorial',
+//    home: MyApp(),
+//  ));
+  runApp(MyApp());
 }
+
+//class MyApp extends StatelessWidget {
+//  @override
+//  Widget build(BuildContext context) {
+//    return MaterialApp(
+//      home: Scaffold(
+//        appBar: AppBar(
+//          title: Text('Flutter layout demo'),
+//        ),
+//        body: Center(
+//          child: Text('Hello world'),
+//        ),
+//      ),
+//    );
+//  }
+//}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Flutter layout demo'),
-        ),
-        body: Center(
-          child: Text('Hello world'),
+    return Container(
+      decoration: BoxDecoration(color: Colors.white),
+      child: Center(
+        child: Text(
+          'Hello World',
+          textDirection: TextDirection.ltr,
+          style: TextStyle(
+            fontSize: 32,
+            color: Colors.black87,
+          ),
         ),
       ),
     );
